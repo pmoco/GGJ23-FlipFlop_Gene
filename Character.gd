2b056a773f9a73b552ff5b6b7ba8bca730cacf59
RicardoEPRodrigues <ricardo.e.p.rodrigues@gmail.com>
2023-02-04 13:35:28 +0000

    Add parenting and auxiliary functions to manage relations.

--- Character.gd
+++ Character.gd
@@ -24,6 +24,11 @@ var is_alive : bool = true
 # this value impacts how a character is able to succeeds in an action.
 var efficiency : float = 0.1
 
+# PARENTING
+var parents = [] # of type Character
+var children = [] # of type Character
+var partner : Character = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _init() -> void:
@@ -40,4 +45,43 @@ func _to_string() -> String:
 	var alive_text = "It is dead."
 	if is_alive:
 		alive_text = "It is alive."
-	return "This character is named " + first_name + " " + last_name + ". It is at the age of " + AGE.keys()[age] + " and is " + STATUS.keys()[status] + ". " + alive_text
+	return "This character is named " + first_name + " " + last_name + ". " + \
+	 "It is at the age of " + AGE.keys()[age] + " and is " + STATUS.keys()[status] + ". " + \
+	 alive_text + \
+	 "It has " + str(num_of_generations()) + " family generations."
+
+func marry(character : Character) -> void:
+	if character == self:
+		# cannot marry yourself
+		return
+		
+	partner = character
+	if character:
+		character.partner = self
+		if num_of_generations() >= character.num_of_generations():
+			character.last_name = last_name
+		else:
+			last_name = character.last_name
+
+func num_of_generations() -> int:
+	# Returns the number of generations the family of this character has.
+	# It only counts from this character upwards
+	var generations = 1
+	
+	var parent_generations = 0
+	for parent in parents:
+		if parent:
+			var gen = parent.num_of_generations()
+			if gen > parent_generations:
+				parent_generations = gen
+	
+	return generations + parent_generations
+
+func is_child_of(character: Character) -> void:
+	if !is_instance_valid(character):
+		return
+	
+	parents.append(character)
+	character.children.append(self)
+	
+	last_name = character.last_name
--- Main.gd
+++ Main.gd
@@ -4,12 +4,81 @@ var characters = []
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	for i in range(10):
-		var chara = Character.new()
-		chara.first_name += "_" + str(i)
-		characters.append(chara)
-		print(chara)
+func _ready():	
+	var A = Character.new()
+	A.first_name = "A"
+	A.last_name = "Fliby"
+	A.age = Character.AGE.ELDER
+	A.status = Character.STATUS.WOUNDED
+	characters.append(A)
+	
+	var B = Character.new()
+	B.first_name = "B"
+	B.last_name = "Flob"
+	B.age = Character.AGE.ELDER
+	characters.append(B)
+	
+	var C = Character.new()
+	C.first_name = "C"
+	C.last_name = "Flab"
+	C.age = Character.AGE.ELDER
+	characters.append(C)
+	
+	B.marry(C)
+
+	var D = Character.new()
+	D.first_name = "D"
+	D.last_name = "Flobady"
+	D.age = Character.AGE.ELDER
+	characters.append(D)
+	
+	A.marry(D)
+	
+	
+	var E = Character.new()
+	E.first_name = "E"
+	E.age = Character.AGE.ADULT
+	characters.append(E)
+	
+	E.is_child_of(B)
+	E.is_child_of(C)
+	
+	var G = Character.new()
+	G.first_name = "G"
+	G.age = Character.AGE.ADULT
+	characters.append(G)
+	
+	G.is_child_of(B)
+	G.is_child_of(C)
+	
+	var H = Character.new()
+	H.first_name = "H"
+	H.age = Character.AGE.ADULT
+	characters.append(H)
+	
+	H.is_child_of(B)
+	H.is_child_of(C)
+	
+	var F = Character.new()
+	F.first_name = "F"
+	F.age = Character.AGE.ADULT
+	characters.append(F)
+	
+	F.is_child_of(A)
+	F.is_child_of(D)
+	
+	H.marry(F)
+	
+	var I = Character.new()
+	I.first_name = "I"
+	I.age = Character.AGE.CHILD
+	characters.append(I)
+	
+	I.is_child_of(H)
+	I.is_child_of(F)
+	
+	for character in characters:
+		print(character)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
