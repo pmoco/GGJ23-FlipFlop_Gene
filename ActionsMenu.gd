77eca7797ef24def36e95692069a992549678993
Tomas Nascimento <gomes.nascimento@tecnico.ulisboa.pt>
2023-02-04 19:55:40 +0000

    Update actionsMenu

---
+++ ActionsMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_Fishing_pressed():
+	pass # Replace with function body.
+
+
+func _on_Hunt_pressed():
+	pass # Replace with function body.
+
+
+func _on_Scavenge_pressed():
+	pass # Replace with function body.
+
+
+func _on_Agriculture_pressed():
+	pass # Replace with function body.
