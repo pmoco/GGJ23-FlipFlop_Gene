d810a53b181f9db43fa468509be52cc3c56c8f8c
Tomas Nascimento <gomes.nascimento@tecnico.ulisboa.pt>
2023-02-04 20:25:42 +0000

    Add label code to ActionsMenu

--- ActionsMenu.gd
+++ ActionsMenu.gd
@@ -1,6 +1,14 @@
-extends Control
+class_name ActionsMenu extends Control
 
-
+func _ready():
+	$Agriculture/Label.text = "Let's plant some potatoes\nrisk : "+"3\n"+"Food: "+"3\n"
+	
+	$Scavenge/Label.text ="Let's get some berries\nrisk : "+"3\n"+"Food: "+"3\n"
+	
+	$Hunt/Label2.text ="Let's hunt some deers\nrisk : "+"3\n"+"Food: "+"3\n"
+	
+	$Fishing/Label.text = "Let's go on relaxing fishing day\nrisk : "+"3\n"+"Food: "+"3\n"
+	
 func _on_Fishing_pressed():
 	pass # Replace with function body.
 
